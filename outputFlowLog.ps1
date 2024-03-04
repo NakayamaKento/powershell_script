e89ebe39f7f199ccb8ba7be21159a0ba746e7d3e
NakayamaKento <40586116+NakayamaKento@users.noreply.github.com>
2024-03-04 04:53:13 +0000

    Add script to log TCP connections to port 3389

---
+++ outputFlowLog.ps1
@@ -0,0 +1,19 @@
+# ログを記録するために、TCP 3389 ポートへの接続を定期的に実行します
+
+# Define an array of server names
+$servers = @(
+    "test-vm-1",
+    "test-vm-2",
+    "test-vm-3"
+)
+
+# Loop through the server list
+Write-Host "Processing server: $server"
+for ($i = 1; $i -le 100; $i++) {
+    foreach ($server in $servers) {
+        # Do something with each server
+        Write-Host "Processing server: $server"
+        Test-NetConnection $server -port 3389
+    }
+    Start-Sleep -Seconds 60
+}
